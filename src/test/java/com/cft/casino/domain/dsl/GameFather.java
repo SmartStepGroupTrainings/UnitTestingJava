516c549cdfca7ed6bfdbce5a124ec68bc38a01cf
Anton Bevzuk <anton.bevzuk@smartstepgroup.com>
2014-09-17 10:12:20 +0400

    Удалил тесты

--- src/test/java/com/cft/casino/domain/dsl/GameFather.java
+++
@@ -1,9 +0,0 @@
-package com.cft.casino.domain.dsl;
-
-import com.cft.casino.domain.RollDiceGame;
-
-public class GameFather {
-    public RollDiceGame please() {
-        return new RollDiceGame();
-    }
-}
