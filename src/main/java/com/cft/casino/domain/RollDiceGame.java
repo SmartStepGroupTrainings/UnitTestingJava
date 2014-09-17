69988ad9c7b0b533e3463cfa72df7e6fbd58bd9d
Anton Bevzuk <anton.bevzuk@smartstepgroup.com>
2014-09-17 11:48:22 +0400

    Упростил хранине ставок в игре Починил тест удалением :)

--- src/main/java/com/cft/casino/domain/RollDiceGame.java
+++ src/main/java/com/cft/casino/domain/RollDiceGame.java
@@ -5,38 +5,32 @@ import java.util.HashMap;
 import java.util.Random;
 
 public class RollDiceGame {
-    private HashMap<Player, ArrayList<Bet>> playersBets = new HashMap<>();
+    private HashMap<Player, Bet> playersBets = new HashMap<>();
     private int bankAmount;
 
     public void addBet(Player player, Bet bet) {
-        if (!playersBets.containsKey(player)) {
-            playersBets.put(player, new ArrayList<>());
-        }
-        playersBets.get(player).add(bet);
+        playersBets.put(player, bet);
     }
 
-    public ArrayList<Bet> getBetsFor(Player player) {
+    public Bet getBetFor(Player player) {
         return playersBets.get(player);
     }
 
     public void play(IDice dice) throws CasinoGameException {
         int winningScore = roll(dice);
 
-        for (HashMap.Entry<Player, ArrayList<Bet>> playerBets : playersBets.entrySet()) {
+        for (HashMap.Entry<Player, Bet> playerBets : playersBets.entrySet()) {
             Player player = playerBets.getKey();
-            for (Bet bet : playerBets.getValue()) {
-                if (bet.getScore() == winningScore) {
-                    player.win(bet.getAmount() * 6);
-                }
-                else {
-                    player.lose();
-                    bankAmount += bet.getAmount();
-                }
+            Bet bet = playerBets.getValue();
+            if (bet.getScore() == winningScore) {
+                player.win(bet.getAmount() * 6);
+            }
+            else {
+                player.lose();
+                bankAmount += bet.getAmount();
             }
         }
-        for (ArrayList<Bet> bets : playersBets.values()) {
-            bets.clear();
-        }
+        playersBets.clear();
     }
 
     private int roll(IDice dice) {
@@ -45,8 +39,6 @@ public class RollDiceGame {
 
     public void leave(Player player) throws CasinoGameException {
         if (!playersBets.containsKey(player)) return;
-        if (playersBets.get(player).size() != 0) throw new CasinoGameException("You can't leave game after bet");
-
         playersBets.remove(player);
     }
 
--- src/test/java/com/cft/casino/domain/PlayerTest.java
+++ src/test/java/com/cft/casino/domain/PlayerTest.java
@@ -3,16 +3,10 @@ package com.cft.casino.domain;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public class PlayerTest {
     @Test
     public void samplePassingTest() {
         assertTrue(true);
-    }
-
-    @Test
-    public void sampleFailingTest() {
-        fail();
     }
 }
