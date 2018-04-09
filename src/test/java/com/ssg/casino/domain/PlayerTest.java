2f4b292b9a17113ab2c604db52a339aab55fcfe4
Seidzi <seidzi.okisame@gmail.com>
2018-04-09 13:39:03 +0600

    added any tests

--- src/test/java/com/ssg/casino/domain/PlayerTest.java
+++ src/test/java/com/ssg/casino/domain/PlayerTest.java
@@ -1,5 +1,6 @@
 package com.ssg.casino.domain;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,5 +9,26 @@ public class PlayerTest {
     @Test
     public void test() {
         assertEquals(1, 1);
+    }
+
+    @Test
+    public void PlayerShouldBuyChips() throws CasinoGameException {
+        Player player = new Player();
+        player.buy(1);
+        Assert.assertEquals(1, player.getAvailableChips());
+    }
+
+    @Test
+    public void PlayerShouldJoinToGame() throws CasinoGameException {
+        Player player = new Player();
+        player.joins(new RollDiceGame());
+        Assert.assertNotNull(player.activeGame());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void PlayerShouldLeaveFromGame() throws CasinoGameException {
+        Player player = new Player();
+        player.leave();
+        Assert.assertNull(player.activeGame());
     }
 }
