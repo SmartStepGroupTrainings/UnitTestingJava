cdec47c094e6622083d32340c48ccdf05c70a032
p1nkZebra <sway4ka@gmail.com>
2018-05-23 12:41:37 +0300

    add behavior test

--- src/test/java/com/ssg/casino/domain/behaviorAndConsistence/WhenSmbdPlayGame.java
+++ src/test/java/com/ssg/casino/domain/behaviorAndConsistence/WhenSmbdPlayGame.java
@@ -4,46 +4,67 @@ import com.ssg.casino.domain.*;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.spy;
 
 public class WhenSmbdPlayGame {
 
     @Test
-    public void loserLooseTheBet() throws CasinoGameException {
-        Player looser = looserWhoBetAllChips();
-
-        looser.activeGame().play();
-
-        assertEquals(0, looser.getAvailableChips());
-    }
-
-    private Player newPlayer() {
-        return new Player();
-    }
-
-    private Player looserWhoBetAllChips() throws CasinoGameException {
-        int allChips = 10;
-        Player player = newPlayer();
-        player.buy(allChips);
-
-        int winningScore = 1;
-        RollDiceGame alwaysLosingGame = newGame(winningScore + 1);
-        player.joins(alwaysLosingGame);
-
-        Bet bet = new Bet(allChips, winningScore);
-        player.bet(bet);
-
-        return player;
-    }
-
-    private RollDiceGame newGame(int winningScore) {
+    public void diceIsRolledOnce() throws CasinoGameException {
         IDice dice = mock(IDice.class);
-        when(dice.roll()).thenReturn(winningScore);
         RollDiceGame diceGame = new RollDiceGame(dice);
 
-        return diceGame;
+        diceGame.play();
+
+        verify(dice, times(1)).roll();
     }
 
+    @Test
+    public void callPlayerWinForAllWinners() throws CasinoGameException {
+        RollDiceGame diceGame = getRollDiceGame();
+
+        Player firstWinner = newWinnerPlayer(diceGame);
+        Player secondWinner = newWinnerPlayer(diceGame);
+        Player looser = newLooserPlayer(diceGame);
+
+
+        diceGame.play();
+
+
+        verify(firstWinner, times(1)).win(anyInt());
+        verify(secondWinner, times(1)).win(anyInt());
+        verify(looser, times(0)).win(anyInt());
+
+    }
+
+    private RollDiceGame getRollDiceGame() {
+        IDice dice = mock(IDice.class);
+        when(dice.roll()).thenReturn(1);
+        return new RollDiceGame(dice);
+    }
+
+
+    private Player newWinnerPlayer(RollDiceGame diceGame) throws CasinoGameException {
+        Player winner = spy(Player.class);
+        winner.buy(10);
+
+        winner.joins(diceGame);
+
+        winner.bet(new Bet(10,1));
+
+        return winner;
+    }
+
+    private Player newLooserPlayer(RollDiceGame diceGame) throws CasinoGameException {
+        Player winner = spy(Player.class);
+        winner.buy(10);
+
+        winner.joins(diceGame);
+
+        winner.bet(new Bet(10,2));
+
+        return winner;
+    }
 
 }
