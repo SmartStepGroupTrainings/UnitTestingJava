7fdfa848c48d8a710d95380b35cf9b6f22db52e4
p1nkZebra <sway4ka@gmail.com>
2018-05-24 13:04:00 +0300

    game can not accept more than six players

--- src/test/java/com/ssg/casino/domain/GameCanNot.java
+++ src/test/java/com/ssg/casino/domain/GameCanNot.java
@@ -9,9 +9,20 @@ public class GameCanNot extends Base{
     @Test(expected = CanNotAddNewPlayerToTheGameException.class)
     public void acceptMoreThanSixPlayers() throws OnlyOneGameException, CanNotAddNewPlayerToTheGameException {
 
+        Game fullGameWithSixPlayers = fullGameWithSixPlayers();
+        Player seventhPlayer = newPlayer();
+
+        seventhPlayer.enter(fullGameWithSixPlayers);
+
+    }
+
+    private Game fullGameWithSixPlayers() throws OnlyOneGameException, CanNotAddNewPlayerToTheGameException {
         Game game = new Game();
-        for (int i = 0; i < 7; i++) {
+
+        for (int i = 0; i < 6; i++) {
             newPlayerInGame(game);
         }
+
+        return game;
     }
 }
